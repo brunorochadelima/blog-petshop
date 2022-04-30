0e224fabba08631659acd2db4f03de4f424d43ce
brunorochadelima <brunorocha33@live.com>
2022-04-30 15:21:59 -0300

    criando primeira rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import './assets/css/base/base.css'
+import React from "react";
+import "./assets/css/base/base.css";
+import Home from "./paginas/Home.jsx";
+import Sobre from "./paginas/Sobre.jsx";
 
 function App() {
-  return (
-  <>
+  const Render = () => {
+    const location = window.location.pathname;
+    if (location == "/sobre") {
+      return <Sobre />;
+    } else {
+      return <Home />;
+    }
+  };
 
-  </>
-  )
+  return <>{Render()}</>;
 }
 
-export default App
+export default App;
