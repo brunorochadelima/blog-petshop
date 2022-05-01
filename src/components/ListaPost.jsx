2e27e2bfbb14566113105fec0db98557dc57f55a
brunorochadelima <brunorocha33@live.com>
2022-05-01 12:19:11 -0300

    renderizando os posts com os dados da api

--- src/components/ListaPost.jsx
+++ src/components/ListaPost.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import {busca} from '../api/api'
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { busca } from "../api/api";
 
-export default function ListaPost() {
+export default function ListaPost({ url }) {
+  const [post, setPost] = React.useState([]);
+
+  React.useEffect(() => {
+    busca(url, setPost);
+  }, [url]);
+
   return (
     <section className="posts container">
-
+      {post.map((post) => (
+        <Link className={`cartao-post cartao-post--${post.categoria}`}>
+          <article key={post.id}>
+            <h3 className="cartao-post__titulo">
+              {post.title}
+              <p className="cartao-post__meta">{post.meta}</p>
+            </h3>
+          </article>
+        </Link>
+      ))}
     </section>
-  )
+  );
 }
