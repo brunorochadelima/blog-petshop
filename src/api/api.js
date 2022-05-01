5f9308a326aa2441bc37fe3de1df3c1436c3bd11
brunorochadelima <brunorocha33@live.com>
2022-05-01 11:27:29 -0300

    conectando com api

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const api = axios.create ( {
+  baseURL: 'http://localhost:5000'
+})
+
+export const busca = async() => {
+  const resposta = await api.get(url)
+  setDado(resposta.data)
+}
+
+//assync para conectar coma api de forma assincrona
+// await é necessário quando usar assync para dizer que quero esperar a resposta da api
---
+++ src/components/ListaPost.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {busca} from '../api/api'
+
+export default function ListaPost() {
+  return (
+    <section className="posts container">
+
+    </section>
+  )
+}
